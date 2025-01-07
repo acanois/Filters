7f7b959eb28ac0c9d1e061bd34c2c5e2c5a4f33d
acanois <david.richter42@gmail.com>
2025-01-07 15:07:46 -0600

    add highpass filter class

---
+++ source/HighPassFilter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by David Richter on 1/7/25.
+//
+
+#include "HighPassFilter.h"
---
+++ source/HighPassFilter.h
@@ -0,0 +1,10 @@
+//
+// Created by David Richter on 1/7/25.
+//
+
+#pragma once
+
+
+class HighPassFilter {
+
+};
